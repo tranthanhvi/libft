6bb3321626c7630a6017d78d270d13b38982719e
Thanh Tran <tranthanhvi66@gmail.com>
2024-11-10 15:21:51 +0200

    test

---
+++ functions/libft.h
@@ -0,0 +1,41 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+// Memory functions
+void *ft_memset(void *pointer, int value, size_t size);
+void *ft_memcpy(void *dest, const void *src, size_t n);
+void *ft_memmove(void *dest, const void *src, size_t n);
+
+
+void    ft_bzero(void *s, size_t n);
+
+// String functions
+size_t ft_strlen(const char *str);
+char    *ft_strdup(const char   *str);
+char    *ft_strcpy(const char   *str);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
+size_t  ft_strlcpy(char *dst, const char *src, size_t size);
+void    ft_putnbr_fd(int n, int fd);
+void    ft_putstr_fd(char *s, int fd);
+
+
+
+
+// Character functions
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+int ft_tolower(int c);
+int ft_toupper(int c);
+void    ft_putchar_fd(char c, int fd);
+
+
+#endif
