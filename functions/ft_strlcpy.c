a3083d0faf46c11a60c6d48d5f3ef7b394ffbd7b
Thanh Tran <tranthanhvi66@gmail.com>
2024-11-14 19:45:36 +0200

    edit strlcpy.c

--- functions/ft_strlcpy.c
+++ functions/ft_strlcpy.c
@@ -16,11 +16,3 @@ size_t  ft_strlcpy(char *dst, const char *src, size_t size)
     }
     return (len);
 }
-/* 
-int main(void)
-{
-    char    dst[13];
-    printf("%u\n", ft_strlcpy(dst, "Hello, World!", 10));
-    printf("%s\n", dst);
-    return (0);
-} */
