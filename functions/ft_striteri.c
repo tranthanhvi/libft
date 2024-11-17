d52bb10bc6520aaa9cdecf486286906fab3568fc
Thanh Tran <tranxanhvi@gmail.com>
2024-11-17 22:55:41 +0200

    normed till ft_striteri.c

--- functions/ft_striteri.c
+++ functions/ft_striteri.c
@@ -10,15 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void    ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    unsigned int    i;
+	unsigned int	i;
 
-    i = 0;
-    if (s && f)
-        while (s[i])
-        {
-            f(i, &s[i]);
-            i++;
-        }
+	i = 0;
+	if (s && f)
+	{
+		while (s[i])
+		{
+			f(i, &s[i]);
+			i++;
+		}
+	}
 }
--- functions/ft_strlcat.c
+++ functions/ft_strlcat.c
@@ -12,29 +12,29 @@
 
 #include "libft.h"
 
-size_t  ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-    char    *d;
-    size_t  dlen;
-    size_t  slen;
-    const char  *s;
+	char		*d;
+	size_t		dlen;
+	size_t		slen;
+	const char	*s;
 
-    d = dst;
-    s = src;
-    dlen = 0;
-    slen = 0;
-    while (size-- && *d)
-    {
-        dlen++;
-        d++;
-    }
-    if (size == 0)
-        return (dlen + slen);
-    while (*s && --size)
-    {
-        *d++ = *s++;
-        slen++;
-    }
-    *d = '\0';
-    return (dlen + slen);
+	d = dst;
+	s = src;
+	dlen = 0;
+	slen = 0;
+	while (size-- && *d)
+	{
+		dlen++;
+		d++;
+	}
+	if (size == 0)
+		return (dlen + slen);
+	while (*s && --size)
+	{
+		*d++ = *s++;
+		slen++;
+	}
+	*d = '\0';
+	return (dlen + slen);
 }
